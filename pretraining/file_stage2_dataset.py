3da19e6260a93baa059c5df273fe1c1ce531dd50
Yutao Zhu <740876080@qq.com>
2021-05-11 11:37:15 +0800

    Add files via upload

---
+++ pretraining/file_stage2_dataset.py
@@ -0,0 +1,80 @@
+import torch
+import linecache
+from torch.utils.data import Dataset
+import numpy as np
+import random
+import re
+    
+class ContrasDataset(Dataset):
+    def __init__(self, filename, max_seq_length, tokenizer, aug_strategy=["sent_deletion", "term_deletion", "qd_reorder"]):
+        super(ContrasDataset, self).__init__()
+        self._filename = filename
+        self._max_seq_length = max_seq_length
+        self._tokenizer = tokenizer
+        self._aug_strategy = aug_strategy
+        self._rnd = random.Random(0)
+        with open(filename, "r") as f:
+            self._total_data = len(f.readlines())
+    
+    def check_length(self, pairlist):
+        max_seq_length = self._max_seq_length - 2
+        if len(pairlist) == 1:
+            pairlist[0].pop(0)
+        else:
+            q_d_minimum_length = 0
+            for i in range(len(pairlist)):
+                q_d_minimum_length += len(pairlist[i]) + 1
+            if q_d_minimum_length > max_seq_length:
+                pairlist.pop(0)
+                pairlist = self.check_length(pairlist)
+        return pairlist
+    
+    def anno_main(self, qd_pairs):
+        all_qd = []
+        for qd in qd_pairs:
+            qd = self._tokenizer.tokenize(qd)
+            all_qd.append(qd)
+        all_qd = self.check_length(all_qd)
+        history_toks = ["[CLS]"]
+        for sent in all_qd:
+            history_toks.extend(sent + ["[eos]"])
+        history_toks += ["[SEP]"]
+        all_qd_toks = history_toks
+        all_attention_mask = [1] * len(all_qd_toks)
+        segment_ids = [0] * len(all_qd_toks)
+        assert len(all_qd_toks) <= self._max_seq_length
+        while len(all_qd_toks) < self._max_seq_length:
+            all_qd_toks.append("[PAD]")
+            segment_ids.append(0)
+            all_attention_mask.append(0)
+        assert len(all_qd_toks) == len(segment_ids) == len(all_attention_mask) == self._max_seq_length
+        anno_seq = self._tokenizer.convert_tokens_to_ids(all_qd_toks)
+        input_ids = np.asarray(anno_seq)
+        all_attention_mask = np.asarray(all_attention_mask)
+        segment_ids = np.asarray(segment_ids)
+        return input_ids, all_attention_mask, segment_ids
+    
+    def __getitem__(self, idx):
+        line = linecache.getline(self._filename, idx + 1)
+        line = line.strip().split("\t")
+        label = int(line[0])
+        qd_pairs = line[1:]
+        history = qd_pairs[:-1]
+        doc = [qd_pairs[-1]]
+
+        input_ids, attention_mask, segment_ids = self.anno_main(history)
+        input_ids2, attention_mask2, segment_ids2 = self.anno_main(doc)
+
+        batch = {
+            'input_ids1': input_ids, 
+            'token_type_ids1': segment_ids, 
+            'attention_mask1': attention_mask, 
+            'input_ids2': input_ids2, 
+            'token_type_ids2': segment_ids2, 
+            'attention_mask2': attention_mask2, 
+        }
+        return batch
+    
+    def __len__(self):
+        return self._total_data
+
