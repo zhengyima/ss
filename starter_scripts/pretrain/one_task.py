5d76bf5c60f4368ce5d53946b7a680cb8d162486
Yutao Zhu <740876080@qq.com>
2021-05-12 16:21:51 +0800

    Add files via upload

--- starter_scripts/pretrain/one_task.py
+++ starter_scripts/pretrain/one_task.py
@@ -79,11 +79,6 @@ def main():
     os.system(f"cd /cache/ss/Pretraining && CUDA_VISIBLE_DEVICES=0,1,2,3 python runBertContras.py --bert_model_path /cache/data/BertModel/ --per_gpu_batch_size 128 --log_path /cache/output/pretraining/logs/ --save_path /cache/output/pretraining/models/ --epochs 5 --temperature 0.1 --aug_strategy term_deletion")
     os.system(f"cd /cache/ss/Pointwise && CUDA_VISIBLE_DEVICES=0,1,2,3 python runBert.py --score_file_path /cache/output/BertContrastive.aol.5.10.128.term_deletion.score.txt --bert_model_path /cache/data/BertModel/ --per_gpu_batch_size 64 --log_path /cache/output/pointwise/logs/ --save_path /cache/output/pointwise/models/bert_sessionsearch.aol.5.10.128.term_deletion --epochs 3 --pretrain_model_path /cache/output/pretraining/models/BertContrastive.aol.5.10.128.term_deletion --learning_rate 5e-5")
     
-    os.system(f"cd /cache/ss/Pretraining && CUDA_VISIBLE_DEVICES=0,1,2,3 python runBertContras.py --bert_model_path /cache/data/BertModel/ --per_gpu_batch_size 128 --log_path /cache/output/pretraining/logs/ --save_path /cache/output/pretraining/models/ --epochs 5 --temperature 0.1 --aug_strategy sent_deletion,term_deletion,qd_reorder")
-    os.system(f"cd /cache/ss/Pointwise && CUDA_VISIBLE_DEVICES=0,1,2,3 python runBert.py --score_file_path /cache/output/BertContrastive.aol.5.10.128.sent_deletion.term_deletion.qd_reorder.score.txt --bert_model_path /cache/data/BertModel/ --per_gpu_batch_size 64 --log_path /cache/output/pointwise/logs/ --save_path /cache/output/pointwise/models/bert_sessionsearch.aol.5.10.128.sent_deletion.term_deletion.qd_reorder --epochs 3 --pretrain_model_path /cache/output/pretraining/models/BertContrastive.aol.5.10.128.sent_deletion.term_deletion.qd_reorder --learning_rate 5e-5")
-    
-
-
     mox.file.copy_parallel('/cache/output', s3_output_path)
     print("write success")
 
